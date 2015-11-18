7151b4209c33e01fd61eacb3fe84108d49e0b5cf
Chris Hitchcott <hitchcott@gmail.com>
2015-11-19 03:20:13 +0800

    fill screen if not filled by one chunkSize

--- both/views/DappsList.jsx
+++ both/views/DappsList.jsx
@@ -50,31 +50,42 @@ DappsList = React.createClass({
   // infinite scrolling
   loadMoreItems() {
     var childCount = $('.col', this.refs.dappSection.getDOMNode()).size()
-    // make sure all items are loaded
     var sessionGetLastResult = Session.get('lastResult');
+    // don't try to load more items until we've matched the last request
     if(childCount >= sessionGetLastResult){
       Session.set('lastResult', sessionGetLastResult + chunkSize);
     }
   },
 
   handleScroll: _.debounce(function(){
-    var $lastItem = $('.col:last-child', this.refs.dappSection.getDOMNode());
     // get the position of `blocksInAdvance` blocks before it ends
+    var $lastItem = $('.col:last-child', this.refs.dappSection.getDOMNode());
     var targetPosition = Math.round($lastItem.offset().top - ($lastItem.height() * blocksInAdvance));
+    var windowHeight = $window.height();
     if($window.scrollTop() + $window.height() >= targetPosition) {
-      this.loadMoreItems()
+      this.loadMoreItems();
     };
   }, 200),
 
+  componentDidUpdate() {
+    // check to see if screen is fully populated
+    var $lastItem = $('.col:last-child', this.refs.dappSection.getDOMNode());
+    if(Math.floor($lastItem.offset().top) + $lastItem.height() < $window.height()){
+      this.loadMoreItems()
+    }
+  },
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
+    this.componentDidUpdate()
   },
+
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   },
 
   scrollToTop() {
-    window.scrollTo(500, 0);
+    window.scrollTo(0, 0);
   },
 
   renderDapps() {
@@ -86,12 +97,27 @@ DappsList = React.createClass({
     });
   },
 
+  renderModal() {
+    return (
+      <div id="modal1" className="modal">
+        <div className="modal-content">
+          <h4>Modal Header</h4>
+          <p>A bunch of text</p>
+        </div>
+        <div className="modal-footer">
+          <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
+      </div>
+    )
+  },
+
   render() {
     return (
       <div>
         <div onClick={this.scrollToTop} className='scroll-to-top'>
           <i className='fa fa-fw fa-arrow-up'></i>
         </div>
+        {this.renderModal()}
         <div ref='navArea' className="header-container container">
           <header className="center-align">
             <h1>State of the Ðapps</h1>
